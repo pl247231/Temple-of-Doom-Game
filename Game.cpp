c7b1fc8a369081c01a31a1146d0fdce0401c2cc5
Alan Wu <70585668+pl247231@users.noreply.github.com>
2024-09-23 18:51:48 -0400

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,12 +1,9 @@
-// Game.cpp
-
 #include "Game.h"
 #include "Level.h"
 #include "utilities.h"
 #include <iostream>
-using namespace std;
 
-// Implement these and other Game member functions you may have added.
+using namespace std;
 
 Game::Game(int goblinSmellDistance) : m_player(1, 1)
 {
@@ -49,9 +46,3 @@ void Game::play()
         }
     }
 }
-
-// You will presumably add to this project other .h/.cpp files for the various
-// classes of objects you need to play the game:  player, monsters, weapons, 
-// etc.  You might have a separate .h/.cpp pair for each class (e.g., Player.h,
-// Bogeyman.h, etc.), or you might put the class declarations for all actors in
-// Actor.h, all game objects in GameObject.h, etc.
